ae0815b88bcea5f5a1e657b0ed0ebc4c99ea02cb
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-14 21:20:23 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i< n; i++) {
+		map <string, int> m;//map 사용
+		int num;
+		cin >> num;
+		int result = 1;
+		for (int j = 0; j < num; j++) {
+			string name, kind;
+			cin >> name >> kind;
+			if (m.find(kind) == m.end()) {//해당값이 없으면 end
+				//m.insert({ kind, 1 });
+				m[kind] = 1;
+			}
+			else {
+				m[kind]++; //존재하면 값 증가
+			}
+		}
+
+		for (auto k : m) {
+			result *= k.second + 1;//종류 개수에 따라
+		}
+		cout << result - 1 << endl;//아무것도 안입은 경우 제외
+	}
+}
