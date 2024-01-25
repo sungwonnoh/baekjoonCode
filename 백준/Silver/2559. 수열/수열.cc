2ae259994c297c0987e106fba219c588e49542b7
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-25 19:56:46 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, k, t;
+	cin >> n >> k;
+
+	vector<int> sum(n+1);
+	sum.push_back(0);
+	for (int i = 1; i <=n; i++) {
+		cin >> t;
+		sum[i] = t + sum[i - 1];
+	}
+
+	int ans = sum[k];
+	for (int i = k; i <= n; i++) {//n-k개
+		ans = max(ans, sum[i] - sum[i - k]);
+	}
+	cout << ans;
+
+	return 0;
+}
